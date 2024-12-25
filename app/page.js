0311db93e247551454f0960193c792daf08bc04b
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-12-25 13:47:08 +0000

    Configrator work in progress

--- app/page.js
+++ app/page.js
@@ -5,15 +5,15 @@ import Configurator from "@/components/Configurator";
 function Home() {
   return (
     <div
-    onContextMenu={(e) => e.preventDefault()}
-    className="mb-5 min-h-screen"
-  >
+      onContextMenu={(e) => e.preventDefault()}
+      className="mb-5 min-h-screen"
+    >
       <Header />
-      <main className="mx-auto flex max-w-7xl flex-col gap-2 px-2 sm:flex-row sm:gap-4">
-        <section className="w-fullsm sm:w-3/4">
+      <main className="mx-auto flex max-w-7xl flex-col gap-2 px-2 md:flex-row md:gap-4">
+        <section className="w-full md:w-3/4">
           <Preview />
         </section>
-        <section className="w-full bg-green-300 sm:w-1/4">
+        <section className="w-full md:w-1/4">
           <Configurator />
         </section>
       </main>
