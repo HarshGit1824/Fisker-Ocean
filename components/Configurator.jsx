d4103203d8b438aed785b655bdf926273dea5498
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-12-25 17:55:16 +0000

    Add React Medium Image Zoom effect and improve responsiveness

--- components/Configurator.jsx
+++ components/Configurator.jsx
@@ -8,9 +8,9 @@ function Configurator() {
         interior and 3 wheel options.
       </p>
       {/* Exterior Color */}
-      <div>
-        <h3>Exterior Color</h3>
-        <div className="flex gap-1">
+      <div className="my-4">
+        <h3 className="mb-2 text-lg font-bold">Exterior Color</h3>
+        <div className="flex flex-wrap gap-2">
           {new Array(6).fill(0).map(
             (
               _,
@@ -18,14 +18,16 @@ function Configurator() {
             ) => (
               <button
                 key={i}
-                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-110`}
+                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
                 onClick={() => console.log(i + 1)}
               >
                 <Image
                   src="/sun_soaked.webp"
                   alt="Color"
-                  width={50}
-                  height={50}
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="w-11"
                 />
               </button>
             ),
@@ -33,7 +35,59 @@ function Configurator() {
         </div>
       </div>
       {/* Wheel Color */}
+      <div className="my-4">
+        <h3 className="mb-2 text-lg font-bold">Wheel Options</h3>
+        <div className="flex gap-2">
+          {new Array(3).fill(0).map(
+            (
+              _,
+              i, // 6 colors
+            ) => (
+              <button
+                key={i}
+                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
+                onClick={() => console.log(i + 1)}
+              >
+                <Image
+                  src="/sun_soaked.webp"
+                  alt="Color"
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="w-14"
+                />
+              </button>
+            ),
+          )}
+        </div>
+      </div>
       {/* Interior Color */}
+      <div className="my-4">
+        <h3 className="mb-2 text-lg font-bold">Interior Color</h3>
+        <div className="flex gap-2">
+          {new Array(2).fill(0).map(
+            (
+              _,
+              i, // 6 colors
+            ) => (
+              <button
+                key={i}
+                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
+                onClick={() => console.log(i + 1)}
+              >
+                <Image
+                  src="/sun_soaked.webp"
+                  alt="Color"
+                  width={0}
+                  height={0}
+                  sizes="100%"
+                  className="w-14"
+                />
+              </button>
+            ),
+          )}
+        </div>
+      </div>
     </>
   );
 }
--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,31 +1,39 @@
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 import Image from "next/image";
 function Preview() {
   return (
     <>
       {/* Exterior Image */}
-      <Image
-        src="/slipstream_black.webp"
-        alt=""
-        width={0}
-        height={0}
-        sizes="100%"
-        className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
-      />
+      <Zoom>
+        <Image
+          src="/slipstream_black.webp"
+          alt=""
+          width={0}
+          height={0}
+          sizes="100%"
+          className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
+        />
+      </Zoom>
       {/* Exterior Description */}
       <section className="my-4 pl-2">
         <h3 className="text-2xl font-bold">Metallic Gloss</h3>
         <p>A rich and luxurious interior with a modern and sleek design.</p>
       </section>
       {/* Interior Image */}
-      <Image
-        src="/slipstream_black.webp"
-        alt=""
-        width={0}
-        height={0}
-        sizes="100%"
-        className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
-      />
+      <Zoom>
+        <Image
+          src="/slipstream_black.webp"
+          alt=""
+          width={0}
+          height={0}
+          sizes="100%"
+          className="h-96 w-full rounded-lg object-cover sm:h-[500px]"
+        />
+      </Zoom>
     </>
   );
 }
 export default Preview;
+
+
