8de4ad16d02a2bd0c4d6bb75985b6787eb7044bc
Harshal Thaware <125807305+HarshGit1824@users.noreply.github.com>
2024-12-25 13:43:16 +0000

    Add preview component

--- app/page.js
+++ app/page.js
@@ -4,10 +4,13 @@ import Preview from "@/components/Preview";
 import Configurator from "@/components/Configurator";
 function Home() {
   return (
-    <div onContextMenu={(e) => e.preventDefault()} className="min-h-screen">
+    <div
+    onContextMenu={(e) => e.preventDefault()}
+    className="mb-5 min-h-screen"
+  >
       <Header />
       <main className="mx-auto flex max-w-7xl flex-col gap-2 px-2 sm:flex-row sm:gap-4">
-        <section className="w-full bg-red-300 sm:w-3/4">
+        <section className="w-fullsm sm:w-3/4">
           <Preview />
         </section>
         <section className="w-full bg-green-300 sm:w-1/4">
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,9 +3,11 @@ import { RiGlobalLine } from "react-icons/ri";
 
 function Header() {
   return (
-    <header className="mb-5">
+    <header className="mb-4">
       <section className="mb-2 bg-gray-200 py-1 text-center text-sm font-medium">
-        <p>All electric &#x2022; Dynamic &#x2022; Driver Range</p>
+        <p>
+          All electric &#x2022; Dynamic &#x2022; Driver Range &#x2022; MKBHD{" "}
+        </p>
       </section>
       <section className="mx-auto flex max-w-7xl justify-between px-2">
         <Image
